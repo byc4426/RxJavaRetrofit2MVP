198875de4c300c4f69e9833be89341aca28a696c
byc4426 <1083551537@qq.com>
2017-11-20 10:40:28 +0800

    first commit

---
+++ app/src/main/java/com/fire/demo/rxjavaretrofit2mvp/mvp/persenter/UserLoginPresenter.java
@@ -0,0 +1,42 @@
+package com.fire.demo.rxjavaretrofit2mvp.mvp.persenter;
+
+import com.fire.demo.rxjavaretrofit2mvp.bean.UserInfo;
+import com.fire.demo.rxjavaretrofit2mvp.mvp.base.BasePresenter;
+import com.fire.demo.rxjavaretrofit2mvp.mvp.model.UserLoginModel;
+import com.fire.demo.rxjavaretrofit2mvp.mvp.view.IUserLoginView;
+
+import java.util.Map;
+
+/**
+ * Created by pc on 2016/9/26.
+ */
+public class UserLoginPresenter extends BasePresenter<IUserLoginView> {
+    private UserLoginModel iUserLoginModel;
+
+    public UserLoginPresenter(IUserLoginView view) {
+        attachView(view);
+        iUserLoginModel = new UserLoginModel();
+    }
+
+    public void login(Map<String, String> maps) {
+        mvpView.showLoading();
+        addSubscription(iUserLoginModel.login(maps, new UserLoginModel.OnLoginListener(){
+            @Override
+            public void loginSuccess(UserInfo userInfo) {
+                mvpView.loginSuccess();
+                if (null != userInfo)
+                    mvpView.saveCommonUserInfo(userInfo);
+            }
+
+            @Override
+            public void loginFailed(int toast, String reason) {
+                mvpView.loginFail(toast, reason);
+            }
+
+            @Override
+            public void requestCompleted() {
+                mvpView.hideLoading();
+            }
+        }));
+    }
+}
